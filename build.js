cd4f60ca126d0e1b81a7a7f23ebba9fbb74600b4
tiencoffee4 <tiencoffee4@gmail.com>
2023-03-11 11:08:33 +0700

    2023/03/11 11:08

--- build.js
+++ build.js
@@ -1,15 +1,27 @@
-process.chdir(__dirname)
+!(async function() {
+   process.chdir(__dirname)
 
-const fs = require('fs-extra')
-const jsYaml = require('js-yaml')
+   const fs = require('fs-extra')
+   const jsYaml = require('js-yaml')
+   const terser = require('terser')
 
-const dist = 'F:/apps/Microsoft VS Code/resources/app/extensions/taxon'
-const filename = 'taxon.tmLanguage'
+   const dist = 'F:/apps/Microsoft VS Code/resources/app/extensions/taxon'
 
-let json = jsYaml.load(fs.readFileSync(`./syntaxes/${filename}.yaml`, 'utf8'))
+   let text, json
 
-fs.ensureDirSync(`${dist}/syntaxes`)
-fs.writeJsonSync(`${dist}/syntaxes/${filename}.json`, json)
+   text = fs.readFileSync(`./syntaxes/taxon.tmLanguage.yaml`, 'utf8')
+   json = jsYaml.load(text)
 
-fs.copySync('./package.json', `${dist}/package.json`)
-fs.copySync('./language-configuration.json', `${dist}/language-configuration.json`)
+   fs.ensureDirSync(`${dist}/syntaxes`)
+   fs.writeJsonSync(`${dist}/syntaxes/taxon.tmLanguage.json`, json)
+
+   text = fs.readFileSync('./src/extension.js', 'utf8')
+   text = (await terser.minify(text)).code
+   fs.writeFileSync(`${dist}/dist/extension.js`, text)
+
+   json = fs.readJsonSync('./package.json')
+   fs.writeJsonSync(`${dist}/package.json`, json)
+
+   json = fs.readJsonSync('./language-configuration.json')
+   fs.writeJsonSync(`${dist}/language-configuration.json`, json)
+})()
